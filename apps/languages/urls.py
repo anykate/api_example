9e5076b79feaeaedbc0757bcab7e199cab9bde30
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-08-06 22:09:34 +0530

    Getting Started With Django REST Framework - code by Pretty Printed

--- apps/languages/urls.py
+++ apps/languages/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 
+router = routers.SimpleRouter()
+router.register('', views.LanguageView)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('languages/', include(router.urls), name='api_language'),
 ]
--- apps/languages/views.py
+++ apps/languages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Language
+from .serializers import LanguageSerializer
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'languages/index.html', {})
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
